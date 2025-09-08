c636ef442a11cc878e5b1152b4a9d14f8ec105d6
kevlarwoodvest <kevinwoodcarpentry@gmail.com>
2025-09-08 01:45:30 -0500

    v1.1

--- calc.py
+++ calc.py
@@ -3,4 +3,4 @@ y = input("how many more would you like? ")
           
 z = int(x) + int(y) 
 
-print(z, "Blind Mice! Yep, they are all blind! Shame! you didn't answer quick enough!")
+print(z, "Blind Mice! Yep, they are all blind! Shame! You didn't answer quick enough!")
