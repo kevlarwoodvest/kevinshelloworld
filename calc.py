de27eed2e9deddeedd61ac1b8ebeed064ba7c07d
kevlarwoodvest <kevinwoodcarpentry@gmail.com>
2025-09-08 01:44:52 -0500

    Blind Mice Calculator v1

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
-print(int(input("what's x? ")) + int(input("what's y? "))) 
+x = input("How many mice do we start with? ")
+y = input("how many more would you like? ")
+          
+z = int(x) + int(y) 
 
-
-
-
-
+print(z, "Blind Mice! Yep, they are all blind! Shame! you didn't answer quick enough!")
